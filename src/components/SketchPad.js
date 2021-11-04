163c8b91f6296aa906c627e089c1cc2ede932bfc
Adnnann <adnanovcina1987@gmail.com>
2021-11-04 23:45:56 +0100

    Addedd black color functionality

--- src/components/SketchPad.js
+++ src/components/SketchPad.js
@@ -4,69 +4,61 @@ import '../index.css'
 
 function SketchPad(){
 
-const [num, setNum] = useState(16);
-const [div, setDiv] = useState([]);
-let [auto, setAutoGen] = useState("");
-
+const [num, setNum] = useState(20);
 useEffect(()=>{
-    setInitValue();
-    autoGen(num);
-    test(num);   
+    setInitValue() 
 },[])
 
-
-
-//let div = [];
-//let auto = "";
-let value = 50;
-
-
 const setInitValue = () => {
+    const value = 16;
     setNum(value);
 }
+let black = "white"
+const blackFun = () =>{
+    black === "white" ? black = "black" : black="white"
+    return black;
+}
+const blackDrawing = (e) =>{
+    e.target.style.backgroundColor =black;
+}
 
-const test = num => {
-    let test = [];
-    for(let i=1;i<=num*num;i++){
-        test[i] = <div key={i} style={{ backgroundColor:"white", width:"100%", height:"100%", borderStyle:"solid"}}></div>
-        setDiv([...div,test]);
-    }
+let div = [];
+for(let i=1;i<=num*num;i++){
+    div[i] = <div key={i} class="div" style={{ backgroundColor:"white", width:"100%", height:"100%", borderStyle:"solid"}} onMouseOver={blackDrawing}></div>
+}
+
+let auto = "";
+for(let i=1;i<=num;i++){
+    auto += "auto ";
+}
+
+const resetButton = () => {
+    
+    div = [];
+    const numPrompt = Number(window.prompt("Enter number between 16 and 100"));
+    setNum(numPrompt)
+    document.querySelectorAll('.div').forEach(x=>x.style.backgroundColor = "white")
+   
     
 
 }
 
-let nesto = "";
-const autoGen = num =>{
-    for(let i=1;i<=num;i++){
-        nesto += "auto ";
-       console.log(nesto)
-}
-setAutoGen(nesto);
-    
-}
 
-const resetBoard = () => {
-    
-    const numDiv= window.prompt("Enter number between 16 and 100 to generate new pad")
-    //setNum(numDiv);
-    autoGen(numDiv);
-    test(numDiv);
-    // console.log(auto);
-}
+
+
+
 
     return(
        <div className="container" >
        <h3 style={{textAlign:"center"}}>Etch-A-Sketch</h3>
      
-        <div style={{marginLeft: "20%"}}>
-            <button className="buttons" onClick={resetBoard} >Reset Board</button>
-            <button className="buttons">Black</button>
+        <div style={{marginLeft:"30%"}}>
+            <button className="buttons" onClick={resetButton} >Reset Board</button>
+            <button className="buttons" onClick={blackFun}>Black</button>
             <button className="buttons">Random Color</button>
             <button className="buttons">Clear Board</button>
         </div>
             
-
-       
         <div className="app" style={{gridTemplateColumns:auto}}>
             { 
                  div.map((item) => {
