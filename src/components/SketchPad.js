c727fa498261989c3cde741b5a12a5df7991a40b
Adnnann <adnanovcina1987@gmail.com>
2021-11-04 15:48:41 +0100

    reset button functionality

--- src/components/SketchPad.js
+++ src/components/SketchPad.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import '../index.css'
 
 function SketchPad(){
 
-let num = 16;
+const [num, setNum] = useState("");
+
+useEffect(()=>{
+    setInitValue();
+},[])
+
+console.log(num)
+
 let div = [];
 let auto = "";
+let value = 16;
+
+
+const setInitValue = () => {
+    setNum(value);
+}
 
     for(let i=1;i<=num*num;i++){
         div[i] = <div key={i} style={{ backgroundColor:"white", width:"100%", height:"100%", borderStyle:"solid"}}></div>
@@ -14,12 +28,18 @@ let auto = "";
         auto += "auto "
     }
 
+const resetBoard = () => {
+    setNum();
+    const num = window.prompt("Enter number between 16 and 100 to generate new pad")
+    setNum(num);
+}
+
     return(
        <div className="container" >
        <h3 style={{textAlign:"center"}}>Etch-A-Sketch</h3>
      
         <div style={{marginLeft: "20%"}}>
-            <button className="buttons">Reset Board</button>
+            <button className="buttons" onClick={resetBoard} >Reset Board</button>
             <button className="buttons">Black</button>
             <button className="buttons">Random Color</button>
             <button className="buttons">Clear Board</button>
